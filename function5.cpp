c5cef82c56f3d1750d4a22585c7421a16588ba98
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-08-28 00:26:48 +0530

    Add files via upload

--- function5.cpp
+++ function5.cpp
@@ -4,9 +4,16 @@ using namespace std;
 int mul(int a,int b){
     return a*b;
 }
+int fact(int n){
+    int ans=1;
+    for(int i=1;i<=n;i++){
+        ans=ans*i;
+    }
+}
 int main(){
 
     int a,b;
     cin>>a>>b;
-    cout<<mul(a,b);
+    cout<<mul(a,b)<<endl;
+    cout<<fact(5);
 }
